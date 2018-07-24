b1ff9dbdd8d7ed99c0e44397bf29617789f3a14e
dczwx5 <dczwx5@126.com>
2018-07-25 01:09:32 +0800

    Signed-off-by: dczwx5 <dczwx5@126.com>

--- note/unity/note.c
+++ note/unity/note.c
@@ -1,3 +1,43 @@
+* Build Setting : 发布设置
+	1.player Setting :
+		1.Resolution and presentation : 
+			1.default is full screen : 
+			2.default is navive resolution : 用当前设备的分辨率
+* Edit 菜单
+	1.Selection : 保持对多物体的选择。没什么用
+	2.Project Setting : 
+		1.Input : 输入控制
+		2.Time : 游戏fps控制
+	3.Render Setting : 全局渲染控制
+		1.fog : 如果不打开fog, 物体不管多远，都会显示在窗口, 如果打开了，则会用fog代替远处的物体
+		2.Ambient light : 全局环境光（非自己创建的光源）
+			1.如果做场景烘培, 则要关闭环境光，不然结果会有出入
+			2.将光的颜色设为0
+	4.Snap Setting : 
+		1.操作场景物体时各操作的步长(moveX,Y,Z, scalc...)
+* Assets : (操作针对Project)
+	1.Import Package / Export Package : 导入导出资源包
+	2.Select Dependencies : 
+		1.选择与选中物体组成的资源
+		2.然后通过显示出的物体(们)全选, 右键可以导出资源包(导出单物体的所有资源)
+* gameobject : (操作针对Scene/hierarchy)
+* Component : 
+	1.添加默认的脚本组件
+	2.Navigation : 导航风格
+	3.Scripte : 自己的script
+* Rendering path : 渲染模式
+	1.vectex Lit : 顶点渲染
+		1.性能最高
+		2.不支持阴影
+	2.forward : 正向渲染
+		1.正向渲染
+		2.可以使用32层(layer)
+		3.只支持一个方向光产生阴影
+	3.deferred lighting :  延迟渲染
+		1.性能最低
+		2.不支持移动端
+		3.不支持抗锯齿
+		4.只能使用4层(layer)
 * 生命周期
     * 加载新关卡时，所有对象都会被销毁，如果不想被销毁，使用DontDestroyOnLoad
     
