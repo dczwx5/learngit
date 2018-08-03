e1d7fe251db48eaf36c4ad3900c61f380d343415
dczwx5 <dczwx5@126.com>
2018-08-03 18:11:25 +0800

    Signed-off-by: dczwx5 <dczwx5@126.com>

---
+++ note/unity/myUnityCode/Assets/note/_Render/_Light/_All.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _All : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       //  MSAA.HDR.PBR(Phyics Base..) Baked.
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- note/unity/readme.txt
+++ note/unity/readme.txt
@@ -1,3 +1,10 @@
 http://docs.manew.com/ : 游戏蛮牛 - unity手册
 world machine
 map magic
+
+客户端功能
+fairyUI - 做系统
+战斗 - 技能 - 等 
+寻路 - 导航网格 - 服务器没有
+资源管理
+框架 - gameFramework
